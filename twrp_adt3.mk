042581f9cf63d4dcf0cf8767484ec0d29a022623
XvSvM <vadiagadia@gmail.com>
2024-11-05 18:38:21 +0300

    Update twrp_adt3.mk

--- twrp_adt3.mk
+++ twrp_adt3.mk
@@ -9,15 +9,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-# Virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
 # Inherit some common Twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
@@ -25,7 +16,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/askey/adt3/device.mk)
 
 PRODUCT_DEVICE := adt3
-PRODUCT_NAME := twrp_adt3
+PRODUCT_NAME := omni_adt3
 PRODUCT_BRAND := ADT-3
 PRODUCT_MODEL := ADT-3
 PRODUCT_MANUFACTURER := askey
