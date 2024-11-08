d4ad72e62f39f8441c24c62b6581ff0f1ae97234
XvSvM <vadiagadia@gmail.com>
2024-11-08 17:00:20 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -95,7 +95,7 @@ PRODUCT_PACKAGES += \
 
 # Otacert
 PRODUCT_EXTRA_RECOVERY_KEYS += \
-    $(DEVICE_PATH)/security/miui_releasekey
+    $(DEVICE_PATH)/security/releasekey.x509
 
 # Hidl Service
 PRODUCT_ENFORCE_VINTF_MANIFEST := true
