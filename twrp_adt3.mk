377808ef8705cecd12845962b8ac4e839587fe4c
XvSvM <vadiagadia@gmail.com>
2024-11-10 13:23:58 +0300

    Update twrp_adt3.mk

--- twrp_adt3.mk
+++ twrp_adt3.mk
@@ -1,21 +1,15 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-#Inherit emulated_storage properties
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
-# Inherit userspace reboot properties
-$(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
-
-# Inherit some common Omni stuff.
+# Inherit some common Twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from adt3 device
