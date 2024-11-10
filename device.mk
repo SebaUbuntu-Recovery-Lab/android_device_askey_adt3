03b9b9d5264515b1516a7d8abba0d905c1e2240c
XvSvM <vadiagadia@gmail.com>
2024-11-10 20:45:15 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -34,8 +34,13 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
-    
 
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libkeymaster41 \
+    libpuresoftkeymasterdevice \
+    libion \
+    libxml2
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
