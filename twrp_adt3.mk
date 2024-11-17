7893cc5ecb975c4d89298c47ca344a77cd67acd3
XvSvM <vadiagadia@gmail.com>
2024-11-17 20:08:30 +0300

    22

--- twrp_adt3.mk
+++ twrp_adt3.mk
@@ -1,25 +1,15 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-<<<<<<< HEAD
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-=======
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-#Inherit emulated_storage properties
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
-# Inherit userspace reboot properties
-$(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
->>>>>>> e350a3acb55c4b7de34644b10a3e7fb8bf2e35f1
 
 # Inherit some common Twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
