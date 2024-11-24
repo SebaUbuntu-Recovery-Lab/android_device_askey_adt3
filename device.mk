399ef91e38c0aad5f23fc13b9931b78740c6aa97
XvSvM <vadiagadia@gmail.com>
2024-11-24 15:13:52 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -17,8 +17,8 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctrl \
